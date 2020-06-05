70c85aa4f67f2f425870ecbd824d75ffdfd0ddff
Faith Korir <faithckorir@gmail.com>
2020-06-05 23:53:18 +0300

    Button added

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: MyApp()
-      ),
+      MaterialApp(home: MyApp()),
     );
 
 class MyApp extends StatefulWidget {
@@ -18,19 +16,36 @@ class _MyAppState extends State<MyApp> {
   int number = 1;
   @override
   Widget build(BuildContext context) {
-    return Scaffold( backgroundColor: Colors.lightBlueAccent,
-      appBar: AppBar(title: Text('Ask Me Anything'),),
-      body: Center(
-        child: Container(
-          child: FlatButton(
-            onPressed: () {
-              setState(() {
-                number=Random().nextInt(5)+1;
-              });
-            },
-            child: Image.asset('images/ball$number.png'),
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+      ),
+      body: Column(
+
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  number = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$number.png'),
+            ),
           ),
-        ),
+          Container(
+          width: 200.0,
+            margin: EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
+            child: RaisedButton(child: Text('Ask',style: TextStyle(fontSize: 18.0),),
+              onPressed: () {
+                print('Raised button pressed');
+              },
+            ),
+          )
+        ],
       ),
     );
   }
